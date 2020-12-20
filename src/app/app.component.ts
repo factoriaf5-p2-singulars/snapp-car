d62195c98d379d5b556371164003518a9210c80f
rglepe <rglepe@gmail.com>
2020-12-20 20:05:20 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'snapp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'snapp-car';
+}
